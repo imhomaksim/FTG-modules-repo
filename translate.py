fb8737f713cbc09ac44a1bbbbc7b09eec0b3d059
p <2711611-hackintosh5@users.noreply.gitlab.com>
2021-05-03 21:01:54 +0000

    Remove pyyandextranslateapi since it no longer exists

--- translate.py
+++ translate.py
@@ -14,8 +14,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# requires: pyyandextranslateapi
-
 import logging
 from Yandex import Translate
 
